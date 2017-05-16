b44c38cf7a8a4708269baeeda3450240a14050e5
Gene Burchette <eburch2@umbc.edu>
2017-05-16 15:27:54 -0400

    functional hiscores

--- hiscores.php
+++ hiscores.php
@@ -3,37 +3,60 @@ include 'classes.php';
 session_start();
 include 'commonUI.php';
 startHTML("View Hiscores");
+include 'scoresAndDeaths.php';
+$db = new OregonTrailDatabase("eritte2", "eritte2");
+$scores = [];
+if ($db->connect()) {
+    $scores = $db->getScores();
+}
+
+
 ?>
-    <div id="div_hiscores">
+    <div id="div_hiscores" class="vertical-layout">
         <br><img src="assets/Separator2.png"><br>
         The Oregon Top Ten
         <br><img src="assets/Separator2.png"><br>
         <br>
 
-        <p class="p_inline">
-            Name<br>
-        </p>
+        <!-- prints the scores as a table -->
+        <table>
+            <tr>
+                <th> Name </th>
+                <th> Points </th>
+            </tr>
 
-        <p class="p_inline">
-            Points<br>
-        </p>
+            <?php 
+            foreach ($scores as $score) {
+            ?>
+                <tr>
+                    <td>
+            <?php
+                        echo $score['name'];
+            ?>
+                    </td>
+                    <td>
+            <?php
+                        echo $score['score'];
+            ?>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
 
-        <p class="p_inline">
-            Rating<br>
-        </p>
         <br><br>
         <button id="btn_points" class="btngrp_main">Would you like to see how points are earned?</button>
     </div>
 
-    <div id="div_hidden">
+    <div id="div_hidden" class="vertical-layout">
         <br><img src="assets/Separator2.png"><br>
         On Arriving in Oregon
         <br><img src="assets/Separator2.png"><br>
-        <p id="p_info1" style="display:inline">
+        <p id="p_info1">
             Your most important resource is the people you have with you. You receive points for each member of your party who arrives safely; you receive more points if they arrive in good health!<br><br>
-            <table>
-                <tr> <th><div id="div_txtblock">Health of Party</div></th> <th>
-                <div id="div_txtblock">Points per Person</div><br></th></tr>
+            <table class="vertical-layout">
+                <tr> <th><div id="div_txtblock" class="horizontal-layout">Health of Party</div></th> <th><div id="div_txtblock" class="horizontal-layout">Points per Person</div></th></tr>
                 <tr> <td>Good</td> <td>500</td> </tr>
                 <tr> <td>Fair</td> <td>400</td> </tr>
                 <tr> <td>Poor</td> <td>300</td> </tr>
@@ -44,7 +67,7 @@ startHTML("View Hiscores");
             The resources you arrive with will help you get started in the new land. You receive points for each item you bring safely to Oregon.<br>
 
             <p class="p_inline">
-                <div style="background-color:white;color:black;height:128px;width:128px;">Health of Party</div><br>
+                <div id="div_txtblock" class="horizontal-layout">Health of Party</div>
                 Wagon<br>
                 Ox<br>
                 Spare wagon part<br>
