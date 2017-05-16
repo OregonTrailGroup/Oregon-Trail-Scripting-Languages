2370905c0fd807201786f846a8c99555bd9aa85f
adamwe1 <adamwe1@umbc.edu>
2017-05-15 21:44:37 -0400

    Update lastriver.php

--- lastriver.php
+++ lastriver.php
@@ -10,7 +10,7 @@ if($money >= 500)
 {
 ?>
 <p>
-	<br>To reach Oregon, you must take the 500 dollar toll road or raft<br><br>
+	<br>To reach Oregon, you may take the 500 dollar toll road or raft<br><br>
 	<a href="end.php?toll=1"><button>Take the toll road</button></a><br><br>
 	<a href="lastraft.php"><button>Take the Raft</button></a><br><br>
 </p>
@@ -19,7 +19,7 @@ if($money >= 500)
 else{
 ?>
 <p>
-	<br>To reach Oregon, you must take the 500 dollar toll road or raft<br><br>
+	<br>To reach Oregon, you may take the 500 dollar toll road or raft<br><br>
 	However, Because you have too little money you must raft<br><br>
 	<a href="lastraft.php"><button>Take the Raft</button></a><br><br>
 </p>
